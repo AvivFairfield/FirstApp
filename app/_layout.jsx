022e985904ca7cc1ae269ed41e045fe956dbf268
Aviv Cohen <avivcohen22@icloud.com>
2024-04-24 01:31:11 +0300

    added auth

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,8 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { SplashScreen, Stack } from "expo-router";
-import { useFonts } from "expo-font";
 import { useEffect } from "react";
+import { useFonts } from "expo-font";
+import "react-native-url-polyfill/auto";
+import { SplashScreen, Stack } from "expo-router";
+
+import GlobalProvider from "../context/GlobalProvider";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
 	const [fontsLoaded, error] = useFonts({
@@ -25,13 +29,26 @@ const RootLayout = () => {
 		}
 	}, [fontsLoaded, error]);
 
+	if (!fontsLoaded) {
+		return null;
+	}
+
 	if (!fontsLoaded && !error) {
 		return null;
 	}
+
 	return (
-		<Stack>
-			<Stack.Screen name="Index" options={{ headerShown: false }} />
-		</Stack>
+		<GlobalProvider>
+			<Stack>
+				<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+				<Stack.Screen name="(auth)" options={{ headerShown: false }} />
+				<Stack.Screen name="index" options={{ headerShown: false }} />
+				<Stack.Screen
+					name="search/[query]"
+					options={{ headerShown: false }}
+				/>
+			</Stack>
+		</GlobalProvider>
 	);
 };
 
