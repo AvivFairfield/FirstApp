1bfb930816939e9417c3ee28eda3ad8934f90551
Aviv Cohen <avivcohen22@icloud.com>
2024-04-27 18:21:38 +0300

    finish

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -15,35 +15,47 @@ import EmptyState from "../../components/EmptyState";
 import { getAllPosts, getLatestPosts } from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
 import VideoCard from "../../components/VideoCard";
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const Home = () => {
 	const { data: posts, refetch } = useAppwrite(getAllPosts);
 	const { data: latestPosts } = useAppwrite(getLatestPosts);
 
-	const [refreshring, setRefreshring] = useState(false);
+	const [refreshing, setRefreshing] = useState(false);
+
 	const onRefresh = async () => {
-		setRefreshring(true);
+		setRefreshing(true);
 		await refetch();
-		setRefreshring(false);
+		setRefreshing(false);
 	};
+	const { user } = useGlobalContext();
 
 	return (
-		<SafeAreaView className="bg-primary h-full">
+		<SafeAreaView className="bg-primary">
 			<FlatList
 				data={posts}
 				keyExtractor={(item) => item.$id}
-				renderItem={({ item }) => <VideoCard video={item} />}
+				renderItem={({ item }) => (
+					<VideoCard
+						title={item.title}
+						thumbnail={item.thumbnail}
+						video={item.video}
+						creator={item.creator.username}
+						avatar={item.creator.avatar}
+					/>
+				)}
 				ListHeaderComponent={() => (
-					<View className="my-6 px-4 space-y-6">
-						<View className="justify-between items-start flex-row mb-6">
+					<View className="flex my-6 px-4 space-y-6">
+						<View className="flex justify-between items-start flex-row mb-6">
 							<View>
 								<Text className="font-pmedium text-sm text-gray-100">
 									Welcome Back
 								</Text>
 								<Text className="text-2xl font-psemibold text-white">
-									Fairfield
+									{user.username}
 								</Text>
 							</View>
+
 							<View className="mt-1.5">
 								<Image
 									source={images.logoSmall}
@@ -52,9 +64,11 @@ const Home = () => {
 								/>
 							</View>
 						</View>
+
 						<SearchInput />
+
 						<View className="w-full flex-1 pt-5 pb-8">
-							<Text className="text-gray-100 text-lg font-pregular mb-3">
+							<Text className="text-lg font-pregular text-gray-100 mb-3">
 								Latest Videos
 							</Text>
 
@@ -65,12 +79,12 @@ const Home = () => {
 				ListEmptyComponent={() => (
 					<EmptyState
 						title="No Videos Found"
-						subtitle="Be the first one to upload!"
+						subtitle="No videos created yet"
 					/>
 				)}
 				refreshControl={
 					<RefreshControl
-						refreshing={refreshring}
+						refreshing={refreshing}
 						onRefresh={onRefresh}
 					/>
 				}
